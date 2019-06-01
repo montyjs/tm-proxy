079046aefa98c3b596f94e4ae4fdb3a00d4bbd59
Trevor Markel <tmarkel6849@gmail.com>
2019-06-01 17:39:09 -0600

    update for final step

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
